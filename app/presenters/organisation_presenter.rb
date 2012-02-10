c6252a27ff341359821f4c2d00f053f23ac220fc
James Stewart <james@jystewart.net>
2012-02-10 20:24:43 +0000

    Inch towards supporting more resource types in the presenter by surviving if a subject doesn't have documents

--- app/presenters/organisation_presenter.rb
+++ app/presenters/organisation_presenter.rb
@@ -21,27 +21,34 @@ class OrganisationPresenter < BasePresenter
     return []
   end
 
+  def type_identifier(d)
+    d.class.name.split("::").first.underscore.singularize
+  end
+
   def as_json(options = {})
     if options[:partial]
       return minimum_for_json
     else
-      {
+      response = {
         response: {
           status: "ok",
           total: subject.documents.count,
           content: minimum_for_json.merge(fields: extra_fields_for_json),
-          relations: relationships_for_json,
-          contents: subject.documents.collect { |d|
-            {
-              title: d.title,
-              type: d.class.name.split("::").first.underscore,
-              updated_at: d.updated_at,
-              uri: "#{request.protocol}#{request.host_with_port}#{public_document_path(d, host: request.host)}",
-              api_uri: "#{request.protocol}#{request.host_with_port}#{public_document_path(d, format: :json, host: request.host)}"
-            }
-          }
+          relations: relationships_for_json
         }
       }
+      if subject.respond_to?(:documents)
+        response[:contents] = subject.documents.collect do |d|
+          {
+            title: d.title,
+            type: type_identifier(d),
+            updated_at: d.updated_at,
+            uri: "#{request.protocol}#{request.host_with_port}#{public_document_path(d, host: request.host)}",
+            api_uri: api_resource_url(resource_type: type_identifier(d), id: d, format: :json, host: request.host)
+          }
+        end
+      end
+      response
     end
   end
 
